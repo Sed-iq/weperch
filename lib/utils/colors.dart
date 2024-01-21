d3780fea83b3e4f9a78ff87dbf22cdb7112e997d
Henryikenna <ikennaunegbu10@gmail.com>
2024-01-21 01:16:49 +0100

    splash screen and landing page

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors{
+  static const primaryBlue = Color(0xFF005E9F);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const darkBlue = Color(0xFF00243C);
+  static const brightOrange = Color(0xFFFFB000);
+  static const lightGray = Color(0xFFE5E9EF);
+  static const darkGray = Color(0xFF838587);
+}
